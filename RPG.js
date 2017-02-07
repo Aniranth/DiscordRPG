7dc8794fc93a02f0b1424cbb0ac42c57b6e34668
Aniranth <aniranth@gmail.com>
2017-02-06 21:41:52 -0800

    Add files via upload
    New code

---
+++ RPG.js
@@ -0,0 +1,61 @@
+var Discordie = require("discordie");
+var Events = Discordie.Events;
+ 
+var client = new Discordie();
+ 
+client.connect({ token: "Mjc4MzkwMTc1ODAzODk5OTA0.C3roMw.Bx8YlafKr34xncXc-yYVPu6cAs0" });
+ 
+client.Dispatcher.on(Events.GATEWAY_READY, e => {
+  console.log("Connected as: " + client.User.username);
+});
+ 
+client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
+  if (e.message.content == "!init") {
+	  var user = client.Users.find(u => u.username == e.message.author.username);
+	  if(!user)
+			return;
+	  user.openDM().then(dm => dm.sendMessage("Welcome to character creation!", true));
+  }
+});
+
+client.Dispatcher.on(Events.VOICE_CHANNEL_JOIN, e => {
+	var channelList = client.Channels.toArray();
+	for(var i = 0; i < channelList.length; i++){
+		if(channelList[i].name == "announcements")
+			var channel = channelList[i];
+	}
+	if(channel != null){
+		channel.sendMessage("User " + e.user.username + " has joined the " + e.channel.name + " channel", true);
+	}
+});
+
+//client.Dispatcher.on(Events.VOICE_CHANNEL_JOIN, e => {
+	//if(e.channel.name == "Timeout Room")
+		
+//});
+
+client.Dispatcher.on(Events.PRESENCE_UPDATE, e => {
+	if(e.user.gameName != e.user.previousGameName && e.user.gameName != null){
+		var channelList = client.Channels.toArray();
+		for(var i = 0; i < channelList.length; i++){
+			if(channelList[i].name == "announcements"){
+				var channel = channelList[i];
+			}
+		}
+		if(channel != null){
+			channel.sendMessage(e.user.username + " has begun playing " + e.user.gameName, true);
+		}
+	}else{
+		var channelList = client.Channels.toArray();
+		for(var i = 0; i < channelList.length; i++){
+			if(channelList[i].name == "announcements"){
+				var channel = channelList[i];
+			}
+		}
+		if(channel != null){
+			if(e.user.previousGameName != null){
+				channel.sendMessage(e.user.username + " has stopped playing " + e.user.previousGameName, true);
+			}
+		}
+	}
+});
