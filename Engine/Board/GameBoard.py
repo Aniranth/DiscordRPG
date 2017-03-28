348feef8546c3d6a29f57cc9a6fd3251f5ecd174
Christopher Stevens <onionchi@gmail.com>
2017-03-28 10:30:09 -0700

    GameBoard init and SpriteHandler inited

--- Engine/Board/GameBoard.py
+++ Engine/Board/GameBoard.py
@@ -1,3 +1,8 @@
+#GameBoard.py
+#Author: Onionchi
+#tabs not spaces
 #Returns png of board's output
 class GameBoard:
-	
+	def __init__(self):
+		self.sp_grabber = SpriteHandler(filename, 32, 32, 32, 32)
+
