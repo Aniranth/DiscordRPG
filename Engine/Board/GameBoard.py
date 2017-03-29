d7623c3d467b1713107e477773a08ef62c94f9ee
Christopher Stevens <onionchi@gmail.com>
2017-03-28 20:50:37 -0700

    Can display image of a board's tiles

--- Engine/Board/GameBoard.py
+++ Engine/Board/GameBoard.py
@@ -3,6 +3,10 @@
 #tabs not spaces
 #Returns png of board's output
 class GameBoard:
-	def __init__(self):
-		self.sp_grabber = SpriteHandler(filename, 32, 32, 32, 32)
-
+    def __init__(self, board_xsize, board_ysize):
+        self.board_builder = BoardBuilder(filename, 32, 32, 32, 32)
+        self.tile_mat = [[0 for x in range(board_xsize)] for y in range(board_ysize)]
+        self.object_mat = [[0 for x in range(board_xsize)] for y in    range(board_ysize)]
+        self.actor_mat = [[0 for x in range(board_xsize)] for y in    range(board_ysize)]
+    def buildBoard(self)
+        
--- Engine/ImageBuilder/SpriteHandler.py
+++ Engine/ImageBuilder/SpriteHandler.py
@@ -2,7 +2,7 @@
 #Functions to get sprite by name or ID of of sprite sheet and tile sheet.
 #Author: Onionchi
 #tabs not spaces
-from PIL import Image
+from PIL import Image, ImageFilter
 class SpriteHandler(object):
     def __init__(self, filename, sprite_xres=16, sprite_yres=16, img_xres=2048, img_yres=2048):
         self.s_xres = sprite_xres
@@ -13,13 +13,20 @@ class SpriteHandler(object):
         self.s_per_col = self.i_yres//self.s_yres
         self.f_name = filename
         self.img = Image.open(filename)
-    def getImg(self,x,y):
-        s_area = (self.s_xres*x,self.s_yres*y,self.s_xres*(1+x),self.s_yres*(1+y))
+#    def getImg(self,x,y):
+#        s_area = (self.s_xres*x,self.s_yres*y,self.s_xres*(1+x),self.s_yres*(1+y))
+#        sprite = self.img.crop(s_area).load()
+#        return sprite
+    def getImg(self,index):
+        x = 0 # index // self.i_xres
+        y = 0 # index % self.i_yres
+        s_area = (self.s_xres * x,self.s_yres * y,self.s_xres * (1+x),self.s_yres * (1+y))
         sprite = self.img.crop(s_area)
+        sprite.load()
         return sprite
+ 
     #TODO: getImg by sprite name table
-    #TODO: getImg by sprite ID table
 #example use:
-#handler = SpriteHandler('/home/onionchi/image.jpg',16,16,32,32)
-#im = handler.getImg(0,0)
+#handler = SpriteHandler('/home/onionchi/DiscordRPG/Assets/grass-1.png',16,16,32,32)
+#im = handler.getImg(0)
 #im.show()
