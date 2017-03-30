cea63e77502a1bbe811be05523441e2f33990c5c
Aniranth <aniranth@gmail.com>
2017-03-29 22:26:26 -0700

    Added the message creation class

---
+++ DiscordIF/messageconstructor.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class MessageConstructor {	
+	static buildMessage(stat, player)
+	{
+		var ability_modifier = Math.floor((stat - 10)/2);
+		//Write custom post for discord
+	}
+}
+
+module.exports = MessageConstructor;
